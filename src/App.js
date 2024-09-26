0f67d5f942a0e48483eb431098d1eb5a5a04233c
five0nit <costea.michael@gmail.com>
2024-09-26 21:51:15 +1000

    Initial commit: Create MikeNet with map placeholder

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to MikeNet</h1>
+      </header>
+      <main>
+        <div id="map" style={{ width: '100%', height: '400px', backgroundColor: '#eee' }}>
+          <p>Map placeholder for future content</p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
